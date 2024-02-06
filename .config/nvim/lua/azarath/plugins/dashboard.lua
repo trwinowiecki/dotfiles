2bcdbf6b79fc9a297ab8eeda8416fbbe739b2d3b
Taylor Schwick <trw0511@gmail.com>
2024-02-05 22:26:13 -0500

    dashboard cleanup

--- .config/nvim/lua/azarath/plugins/dashboard.lua
+++ .config/nvim/lua/azarath/plugins/dashboard.lua
@@ -15,20 +15,8 @@ return {
 						icon_hl = "@variable",
 						desc = "Files",
 						group = "Label",
-						action = "Telescope find_files",
+						action = "Telescope git_files",
 						key = "f",
-					},
-					{
-						desc = " Apps",
-						group = "DiagnosticHint",
-						action = "Telescope app",
-						key = "a",
-					},
-					{
-						desc = " dotfiles",
-						group = "Number",
-						action = "Telescope dotfiles",
-						key = "d",
 					},
 				},
 			},
@@ -36,55 +24,3 @@ return {
 	end,
 	dependencies = { { "nvim-tree/nvim-web-devicons" } },
 }
-
--- return {
--- 	"goolord/alpha-nvim",
--- 	dependencies = {
--- 		"nvim-tree/nvim-web-devicons",
--- 	},
---
--- 	config = function()
--- 		local alpha = require("alpha")
--- 		local dashboard = require("alpha.themes.dashboard")
---
--- 		dashboard.section.header.val = {
--- 			[[                                                                       ]],
--- 			[[                                                                       ]],
--- 			[[                                                                       ]],
--- 			[[                                                                       ]],
--- 			[[                                                                       ]],
--- 			[[                                                                       ]],
--- 			[[                                                                       ]],
--- 			[[                                                                     ]],
--- 			[[       ████ ██████           █████      ██                     ]],
--- 			[[      ███████████             █████                             ]],
--- 			[[      █████████ ███████████████████ ███   ███████████   ]],
--- 			[[     █████████  ███    █████████████ █████ ██████████████   ]],
--- 			[[    █████████ ██████████ █████████ █████ █████ ████ █████   ]],
--- 			[[  ███████████ ███    ███ █████████ █████ █████ ████ █████  ]],
--- 			[[ ██████  █████████████████████ ████ █████ █████ ████ ██████ ]],
--- 			[[                                                                       ]],
--- 			[[                                                                       ]],
--- 			[[                                                                       ]],
--- 		}
---
--- 		dashboard.section.buttons.val = {
--- 			dashboard.button("e", "New file", "<cmd>ene <CR>"),
--- 			dashboard.button("SPC f f", "Find file"),
--- 		}
--- 		_Gopts = {
--- 			position = "center",
--- 			hl = "Type",
--- 			-- wrap = "overflow";
--- 		}
---
--- 		-- local function footer()
--- 		-- 	return "Programming is an art of patience not talent"
--- 		-- end
---
--- 		-- dashboard.section.footer.val = footer()
---
--- 		dashboard.opts.opts.noautocmd = true
--- 		alpha.setup(dashboard.opts)
--- 	end,
--- }
