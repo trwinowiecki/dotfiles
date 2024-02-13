01ffae96b216c0ff9ce403edd36fa64896e6565a
Taylor Schwick <trw0511@gmail.com>
2024-02-12 20:44:11 -0500

    add ts organize imports

--- .config/nvim/lua/azarath/plugins/lsp-config.lua
+++ .config/nvim/lua/azarath/plugins/lsp-config.lua
@@ -20,11 +20,26 @@ return {
 
 			local lspconfig = require("lspconfig")
 
+			local function organize_imports()
+				local params = {
+					command = "_typescript.organizeImports",
+					arguments = { vim.api.nvim_buf_get_name(0) },
+					title = "",
+				}
+				vim.lsp.buf.execute_command(params)
+			end
+
 			lspconfig.lua_ls.setup({
 				capabilities = capabilities,
 			})
 			lspconfig.tsserver.setup({
 				capabilities = capabilities,
+				commands = {
+					OrganizeImports = {
+						organize_imports,
+						description = "Organize Imports",
+					},
+				},
 			})
 			lspconfig.tailwindcss.setup({
 				capabilities = capabilities,
